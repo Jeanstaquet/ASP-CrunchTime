fe0080b7c9f9d7d3bb334c28bcc9e7a05fd02c9b
Jeansta <jean.staquet8@gmail.com>
2022-02-11 00:55:33 +0000

    Add more content for the Explore Page

--- src/containers/Explore/Explore.js
+++ src/containers/Explore/Explore.js
@@ -1,50 +1,83 @@
 import React from 'react'
-
+import './explore.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faStar } from '@fortawesome/free-solid-svg-icons'
+import img1 from '../../assets/images/img1.jpeg'
 const Explore = () => {
     return (
-        <div>
-            <div>
-                <div>Icon</div>
-                <div>Explore</div>
-                <div>Icon2</div>
+        <div className="container_Explore">
+            <div className="banner_Explore">
+                <div className="banner_titleContainer_Explore">
+                    <div className="bannerIcon_Explore">CT</div>
+                    <div className="bannerTitle_Explore">Explore</div>
+                </div>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
             </div>
-            <div>
+            <div className="buttons_Explore">
                 <div>Newest first</div>
                 <button>FIND A RECIPE</button>
             </div>
-            <div>
-                <div>
-                    <div>IMG</div>
-                    <div>Chicken Curry</div>
-                    <div>Description</div>
-                    <div>
-                        <div>Starts</div>
+            <div className="recipeListTop_Explore">
+                <div className="recipeItemTop_Explore">
+                    <img src={img1} />
+                    <div className="recipeItemTitleTop_Explore">
+                        Chicken Curry
+                    </div>
+                    <div className="recipeItemDescTop_Explore">Description</div>
+                    <div className="recipeItemStarContTop_Explore">
+                        <div className="stars_Explore">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                        </div>
                         <div>1234</div>
                     </div>
                 </div>
-                <div>
-                    <div>IMG</div>
-                    <div>Chicken Curry</div>
-                    <div>Description</div>
-                    <div>
-                        <div>Starts</div>
+                <div className="recipeItemTop_Explore">
+                    <img src={img1} />
+                    <div className="recipeItemTitleTop_Explore">
+                        Chicken Curry
+                    </div>
+                    <div className="recipeItemDescTop_Explore">Description</div>
+                    <div className="recipeItemStarContTop_Explore">
+                        <div className="stars_Explore">
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                            <FontAwesomeIcon icon={faStar} />
+                        </div>
                         <div>1234</div>
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <div>IMG</div>
+            <hr></hr>
+            <div className="recipeListMid_Explore">
+                <div className="recipeItemMid_Explore">
+                    <img src={img1} />
                     <div>Seasonal Recipies</div>
                 </div>
-                <div>
-                    <div>IMG</div>
+                <div className="recipeItemMid_Explore">
+                    <img src={img1} />
+                    <div>Seasonal Recipies</div>
+                </div>
+                <div className="recipeItemMid_Explore">
+                    <img src={img1} />
                     <div>Seasonal Recipies</div>
                 </div>
             </div>
-            <div>
-                <div>IMG1</div>
-                <div>IMG2</div>
+            <hr></hr>
+            <div className="recipeListBottom_Explore">
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
+                <img src={img1} />
             </div>
         </div>
     )
