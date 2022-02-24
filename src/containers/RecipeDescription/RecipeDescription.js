1141b3d874025bbe06114c449658c69bc27d796b
Natalya <65422154+natalyaJk@users.noreply.github.com>
2022-02-24 14:00:07 -0800

    Small edits to css
    added a basic scrollbar (which will need more work later), and fixed some spacing issues with the topbar

--- src/containers/RecipeDescription/RecipeDescription.js
+++ src/containers/RecipeDescription/RecipeDescription.js
@@ -8,6 +8,7 @@ import smallLogo from '../../images/smallLogo.png'
 import img1 from '../../assets/images/img1.jpeg'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { Card } from 'react-bootstrap'
+import * as Scroll from 'react-scroll';
 
 
 
@@ -20,7 +21,6 @@ const RecipeDescription = () => {
                     <img className='smallLogo' src={smallLogo} alt="CT logo"/>
                     <div className="recipeName" id='recipe-name'>Recipe Name</div>
                     <div className="recipeName" id='author-name'>Author Name</div>
-             
                     <FontAwesomeIcon icon={faArrowLeftLong} />
                 </Navbar>
             </div>
@@ -28,62 +28,67 @@ const RecipeDescription = () => {
             <div className="recipePhoto">
                 <img className='recipeImg' src={img1} alt="Pretty food"/>
             </div>
-            <div className="Time">
-                <div className ="PrepTime">Prep Time:</div>
-                <div className ="CookTime">Cook Time:</div>
-                <div className ="OverallTime">Overall Time:</div>
-            </div>
+            <div className='recipe-body'>
+                <div className="Time">
+                    <div className ="PrepTime">Prep Time:</div>
+                    <div className ="CookTime">Cook Time:</div>
+                    <div className ="OverallTime">Overall Time:</div>
+                </div>
 
-            <div className="ingredientList">
-                <div className='sectionTitle'>Here's what you'll need!</div>
-                <div className='list'>
-                    <ul>
-                        <li>Cream cheese</li>
-                        <li>Parmesan</li>
-                    </ul>
+                <div className="ingredientList">
+                    <div className='sectionTitle'>Here's what you'll need!</div>
+                    <div className='list'>
+                        <ul>
+                            <li>Noodles</li>
+                            <li>Spinach</li>
+                            <li>Cherry tomatoes</li>
+                            <li>Tofu</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="instructions">
-                <div className='sectionTitle'>How to make it:</div>
-                <div className='list'>
-                    <ol>
-                        <li>First you do a thing</li>
-                        <li>Then you do another thing</li>
-                        <li>And there's some other stuff after that</li>
-                        <li>Thought you were done? lol no there's more but trust me it pays off </li>
-                        <li>Do one last thing. Then salt to taste, plate it up real nice, take a photo to prove you cooked
-                        and......now you can eat :) </li>
-                    </ol>
+                <div className="instructions">
+                    <div className='sectionTitle'>How to make it:</div>
+                    <div className='list'>
+                        <ol>
+                            <li>First you do a thing</li>
+                            <li>Then you do another thing</li>
+                            <li>And there's some other stuff after that</li>
+                            <li>Thought you were done? lol no there's more but trust me it pays off </li>
+                            <li>Do one last thing. Then salt to taste, plate it up real nice, take a photo to prove you cooked
+                            and......now you can eat :) </li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
-            <div className="ratings">
-                <div className="stars">
-                    <FontAwesomeIcon id='total-star1' icon={faStar} />
-                    <FontAwesomeIcon id='total-star2' icon={faStar} />
-                    <FontAwesomeIcon id='total-star3' icon={faStar} />
-                    <FontAwesomeIcon id='total-star4' icon={faStar} />
-                    <FontAwesomeIcon id='total-star5' icon={faStar} />
+                <div className="ratings">
+                    <div className="stars">
+                        <FontAwesomeIcon id='total-star1' icon={faStar} />
+                        <FontAwesomeIcon id='total-star2' icon={faStar} />
+                        <FontAwesomeIcon id='total-star3' icon={faStar} />
+                        <FontAwesomeIcon id='total-star4' icon={faStar} />
+                        <FontAwesomeIcon id='total-star5' icon={faStar} />
+                    </div>
+                </div>
+            
+                <div className='review'>
+                    <Card className = "review-card">
+                        <Nav>
+                            <div className='user'>Someone's name</div>
+                            <div className='user-rating'>
+                                <FontAwesomeIcon id='star1' icon={faStar} />
+                                <FontAwesomeIcon id='star2' icon={faStar} />
+                                <FontAwesomeIcon id='star3' icon={faStar} />
+                                <FontAwesomeIcon id='star4' icon={faStar} />
+                                <FontAwesomeIcon id='star5' icon={faStar} />
+                            </div>
+                        </Nav>
+                        
+                        <div className='review-text'>
+                            <p>Here's some reasons why this person liked or disliked the recipe</p>
+                        </div>
+                    </Card>
                 </div>
             </div>
            
-            <div className='review'>
-                <Card className = "review-card">
-                    <Nav>
-                        <div className='user'>Someone's name</div>
-                        <div className='user-rating'>
-                            <FontAwesomeIcon id='star1' icon={faStar} />
-                            <FontAwesomeIcon id='star2' icon={faStar} />
-                            <FontAwesomeIcon id='star3' icon={faStar} />
-                            <FontAwesomeIcon id='star4' icon={faStar} />
-                            <FontAwesomeIcon id='star5' icon={faStar} />
-                        </div>
-                    </Nav>
-                    
-                    <div className='review-text'>
-                        <p>Here's some reasons why this person liked or disliked the recipe</p>
-                    </div>
-                </Card>
-            </div>
         </div>
     )
 }
