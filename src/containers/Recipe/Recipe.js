01d1dd60b70c273ca08200c86d8122cb1a0688db
Jeansta <jean.staquet8@gmail.com>
2022-03-03 22:51:04 +0000

    Add a scroll bar

--- src/containers/Recipe/Recipe.js
+++ src/containers/Recipe/Recipe.js
@@ -10,35 +10,40 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { Card } from 'react-bootstrap'
 //import * as Scroll from 'react-scroll';
 
-
-
-
 const Recipe = () => {
     return (
-        
         <div className="Recipe-Page">
             <div className="topBar">
-                <Navbar expand = "lg" variant="light" bg="light">
-                    <img className='smallLogo' id='CTlogo'src={smallLogo} alt="CT logo"/>
-                    <div className="recipeName" id='recipe-name'>Recipe Name</div>
-                    <div className="recipeName" id='author-name'>Author Name</div>
+                <Navbar expand="lg" variant="light" bg="light">
+                    <img
+                        className="smallLogo"
+                        id="CTlogo"
+                        src={smallLogo}
+                        alt="CT logo"
+                    />
+                    <div className="recipeName" id="recipe-name">
+                        Recipe Name
+                    </div>
+                    <div className="recipeName" id="author-name">
+                        Author Name
+                    </div>
                     <FontAwesomeIcon icon={faArrowLeftLong} />
                 </Navbar>
             </div>
-        
+
             <div className="recipePhoto">
-                <img className='recipeImg' src={img1} alt="Pretty food"/>
+                <img className="recipeImg" src={img1} alt="Pretty food" />
             </div>
-            <Container fluid="md" className='recipe-body'>
+            <Container fluid="md" className="recipe-body">
                 <div className="Time">
-                    <div className ="PrepTime">Prep Time:</div>
-                    <div className ="CookTime">Cook Time:</div>
-                    <div className ="OverallTime">Overall Time:</div>
+                    <div className="PrepTime">Prep Time:</div>
+                    <div className="CookTime">Cook Time:</div>
+                    <div className="OverallTime">Overall Time:</div>
                 </div>
 
                 <div className="ingredientList">
-                    <div className='sectionTitle'>Here's what you'll need!</div>
-                    <div className='list'>
+                    <div className="sectionTitle">Here's what you'll need!</div>
+                    <div className="list">
                         <ul>
                             <li>Noodles</li>
                             <li>Spinach</li>
@@ -48,49 +53,57 @@ const Recipe = () => {
                     </div>
                 </div>
                 <div className="instructions">
-                    <div className='sectionTitle'>How to make it:</div>
-                    <div className='list'>
+                    <div className="sectionTitle">How to make it:</div>
+                    <div className="list">
                         <ol>
                             <li>First you do a thing</li>
                             <li>Then you do another thing</li>
                             <li>And there's some other stuff after that</li>
-                            <li>Thought you were done? lol no there's more but trust me it pays off </li>
-                            <li>Do one last thing. Then salt to taste, plate it up real nice, take a photo to prove you cooked
-                            and......now you can eat :) </li>
+                            <li>
+                                Thought you were done? lol no there's more but
+                                trust me it pays off{' '}
+                            </li>
+                            <li>
+                                Do one last thing. Then salt to taste, plate it
+                                up real nice, take a photo to prove you cooked
+                                and......now you can eat :){' '}
+                            </li>
                         </ol>
                     </div>
                 </div>
                 <div className="ratings">
                     <div className="stars">
-                        <FontAwesomeIcon id='total-star1' icon={faStar} />
-                        <FontAwesomeIcon id='total-star2' icon={faStar} />
-                        <FontAwesomeIcon id='total-star3' icon={faStar} />
-                        <FontAwesomeIcon id='total-star4' icon={faStar} />
-                        <FontAwesomeIcon id='total-star5' icon={faStar} />
+                        <FontAwesomeIcon id="total-star1" icon={faStar} />
+                        <FontAwesomeIcon id="total-star2" icon={faStar} />
+                        <FontAwesomeIcon id="total-star3" icon={faStar} />
+                        <FontAwesomeIcon id="total-star4" icon={faStar} />
+                        <FontAwesomeIcon id="total-star5" icon={faStar} />
                     </div>
                 </div>
-            
-                <div className='review'>
-                    <Card className = "review-card">
+
+                <div className="review">
+                    <Card className="review-card">
                         <Nav>
-                            <div className='user'>Someone's name</div>
-                            <div className='user-rating'>
-                                <FontAwesomeIcon id='star1' icon={faStar} />
-                                <FontAwesomeIcon id='star2' icon={faStar} />
-                                <FontAwesomeIcon id='star3' icon={faStar} />
-                                <FontAwesomeIcon id='star4' icon={faStar} />
-                                <FontAwesomeIcon id='star5' icon={faStar} />
+                            <div className="user">Someone's name</div>
+                            <div className="user-rating">
+                                <FontAwesomeIcon id="star1" icon={faStar} />
+                                <FontAwesomeIcon id="star2" icon={faStar} />
+                                <FontAwesomeIcon id="star3" icon={faStar} />
+                                <FontAwesomeIcon id="star4" icon={faStar} />
+                                <FontAwesomeIcon id="star5" icon={faStar} />
                             </div>
                         </Nav>
-                        
-                        <div className='review-text'>
-                            <p>Here's some reasons why this person liked or disliked the recipe</p>
+
+                        <div className="review-text">
+                            <p>
+                                Here's some reasons why this person liked or
+                                disliked the recipe
+                            </p>
                         </div>
                     </Card>
                 </div>
             </Container>
-           
         </div>
     )
 }
-export default Recipe;
+export default Recipe
