b1e3ee15a2a343a4c619490a7df15a1b3b3b950d
Jeansta <jean.staquet8@gmail.com>
2022-03-08 22:49:25 +0000

    Remove eslint rule

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'no-mixed-spaces-and-tabs': 0,
+        'react/prop-types': 0,
+    },
+}
