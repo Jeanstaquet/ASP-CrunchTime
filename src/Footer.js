dea64706ced1ae33d8d45af0a1d7416265a18738
Jonathan West <gtg393w@gmail.com>
2022-03-08 19:02:17 -0500

    Updated footer functionality

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,3 @@
-import {useState, useEffect} from 'react';
-//import {Link} from 'react-router-dom';
 const sun = require('./assets/images/sun.png');
 const moon = require('./assets/images/moon.png');
 const heart = require('./assets/images/heart.png');
@@ -7,7 +5,6 @@ const stick = require('./assets/images/stick.png');
 
 const Footer = (props) => {
 	const handleClick = (e) => {
-		//console.log(props.bgc);
 		var colour;
 		switch(props.bgc) {
 			case 'white':
@@ -16,19 +13,16 @@ const Footer = (props) => {
 			case 'lightgrey':
 				colour = 'white';
 		}
-		//const colour = (props.bgc === 'white') ? 'lightgrey' : 'white';
-		//console.log(colour);
 		return props.bgcFunc(colour);
 	}
 
-	// useEffect(() => {
-	// 	const style = document.querySelector(".phone").style;
-	// 	console.log(style);
-	// })
-
 	return (
 		<ul className="footer">
-			<li onClick={handleClick}><img className = "picture" src={sun}/></li>
+			{(props.bgc === 'lightgrey') ? (
+				<li onClick={handleClick}><img className = "picture" src={sun}/></li>
+			) : (
+				<li onClick={handleClick}><img className = "picture" src={moon}/></li>
+			)}
 			<li><img className = "picture" src={heart}/></li>
 			<li><img className = "picture" src={stick}/></li>
 		</ul>
